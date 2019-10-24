86320a2dfa144c993231820e3cbf42c7ecd9e09f
Jakob <jakob.lindqvist@chalmers.se>
2019-10-24 19:32:19 +0200

    Added misc w/ tests

--- src/misc.rs
+++ src/misc.rs
@@ -1,6 +1,121 @@
 use serde;
 use serde::Deserialize;
+use serde::Deserializer;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "UPPERCASE")]
-pub struct Misc {}
+pub struct Misc {
+    name: String,
+    version: u8,
+    #[serde(rename = "TYPE")]
+    type_: Type,
+    #[serde(rename = "USE")]
+    use_: Use,
+    time: f32,
+    amount: f32,
+    amount_is_weight: Option<bool>,
+    use_for: Option<String>,
+    notes: Option<String>,
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+enum Use {
+    Boil,
+    Mash,
+    Primary,
+    Secondary,
+    Bottling,
+}
+
+#[derive(Debug, PartialEq)]
+enum Type {
+    Spice,
+    Fining,
+    WaterAgent,
+    Herb,
+    Flavor,
+    Other,
+}
+
+impl<'de> Deserialize<'de> for Type {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        match s.as_str() {
+            "Spice" => Ok(Type::Spice),
+            "Fining" => Ok(Type::Fining),
+            "Water Agent" => Ok(Type::WaterAgent),
+            "Herb" => Ok(Type::Herb),
+            "Flavor" => Ok(Type::Flavor),
+            "Other" => Ok(Type::Other),
+            _ => Err(serde::de::Error::unknown_variant(&s, &["Unknown type"])),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_xml_rs;
+    #[test]
+    fn irish_moss_minimal_fields() {
+        let xml_input = r"
+            <MISC>
+                <NAME>Irish Moss</NAME>
+                <VERSION>1</VERSION>
+                <TYPE>Fining</TYPE>
+                <USE>Boil</USE>
+                <TIME>15.0</TIME>
+                <AMOUNT>0.010</AMOUNT>
+            </MISC>
+        ";
+        let parsed_misc: Misc = serde_xml_rs::from_str(xml_input).unwrap();
+        let true_misc = Misc {
+            name: "Irish Moss".into(),
+            version: 1,
+            type_: Type::Fining,
+            use_: Use::Boil,
+            time: 15.0,
+            amount: 0.010,
+            amount_is_weight: None,
+            use_for: None,
+            notes: None,
+        };
+        assert_eq!(parsed_misc, true_misc);
+    }
+
+    #[test]
+    ///If you add coriander, I will call the police
+    fn coriander_spice_with_typical_fields() {
+        let xml_input = r"
+            <MISC>
+                <NAME>Coriander</NAME>
+                <TYPE>Spice</TYPE>
+                <VERSION>1</VERSION>
+                <USE>Boil</USE>
+                <TIME>5.0</TIME>
+                <AMOUNT>0.025</AMOUNT>
+                <USE_FOR>Belgian Wit Spice</USE_FOR>
+                <NOTES>Used in Belgian Wit, Whites, and Holiday ales.  Very good when used in light wheat ales.  Often used with Bitter Orange Peel. Crack open seeds and add at the end of the boil to extract aroma and flavor.
+                </NOTES>
+            </MISC>
+        ";
+        let parsed_misc: Misc = serde_xml_rs::from_str(xml_input).unwrap();
+        let true_misc = Misc {
+            name: "Coriander".into(),
+            version: 1,
+            type_: Type::Spice,
+            use_: Use::Boil,
+            time: 5.0,
+            amount: 0.025,
+            amount_is_weight: None,
+            use_for: Some("Belgian Wit Spice".into()),
+            notes: Some(
+                "Used in Belgian Wit, Whites, and Holiday ales.  Very good when used in light wheat ales.  Often used with Bitter Orange Peel. Crack open seeds and add at the end of the boil to extract aroma and flavor."
+                .into()),
+        };
+        assert_eq!(parsed_misc, true_misc);
+    }
+}
