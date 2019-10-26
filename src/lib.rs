88d8a4473853ff6d10a8fb363adcc9582c8a6be3
Jakob <jakob.lindqvist@chalmers.se>
2019-10-26 11:56:48 +0200

    Units + mash_step

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,12 @@ pub type IBU = f32;
 pub type SRMColor = f32;
 pub type VolumesCO2 = f32;
 pub type ABV = Percent;
+/// Celsius
+pub type Temperature = f32;
+/// Liters
+pub type Volume = f32;
+/// Minutes
+pub type Time = f32;
 
 pub trait RecordSet {}
 
--- src/mash.rs
+++ src/mash.rs
@@ -1,8 +1,9 @@
 use crate::utils;
+use crate::{Temperature, Time, Volume};
 use serde;
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "UPPERCASE")]
 pub struct Mash {
     name: String,
@@ -20,5 +21,56 @@ pub struct Mash {
     equip_adjust: Option<bool>,
 }
 
-#[derive(Deserialize, Debug)]
-pub struct MashStep {}
+#[derive(Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "UPPERCASE")]
+#[serde(rename = "MASH_STEP")]
+pub struct MashStep {
+    name: String,
+    version: u8,
+    #[serde(rename = "TYPE")]
+    type_: Type,
+    infuse_amount: Option<Volume>,
+    step_temp: Temperature,
+    step_time: Time,
+    ramp_time: Option<Time>,
+    end_temp: Option<Temperature>,
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+enum Type {
+    Infusion,
+    Temperature,
+    Decoction,
+}
+
+#[cfg(test)]
+/// Official tests from 'http://www.beerxml.com/beerxml.htm'
+mod beerxml {
+    use super::*;
+    use serde_xml_rs;
+    #[test]
+    fn infusion_step() {
+        let xml_input = r"
+            <MASH_STEP>
+                <NAME>Conversion step</NAME>
+                <VERSION>1</VERSION>
+                <TYPE>Infusion</TYPE>
+                <STEP_TEMP>68.0</STEP_TEMP>
+                <STEP_TIME>70.0</STEP_TIME>
+                <INFUSE_AMOUNT>5.0</INFUSE_AMOUNT>
+            </MASH_STEP>
+            ";
+        let parsed_mash_step: MashStep = serde_xml_rs::from_str(xml_input).unwrap();
+        let true_mash_step = MashStep {
+            name: "Conversion step".into(),
+            version: 1,
+            type_: Type::Infusion,
+            step_temp: 68.0,
+            step_time: 70.0,
+            infuse_amount: Some(5.0),
+            ramp_time: None,
+            end_temp: None,
+        };
+        assert_eq!(parsed_mash_step, true_mash_step);
+    }
+}
