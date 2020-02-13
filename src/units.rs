b9f164584a064d14f71b609840cf486e4bad366f
Jakob Lindqvist <jakob.lindqvist.92@gmail.com>
2020-02-13 16:02:35 +0100

    Units

---
+++ src/units.rs
@@ -0,0 +1,14 @@
+pub type Percent = f32;
+pub type PartsPerMillion = f32;
+pub type PH = f32;
+pub type SpecificGravity = f32;
+pub type IBU = f32;
+pub type SRMColor = f32;
+pub type VolumesCO2 = f32;
+pub type ABV = Percent;
+/// Celsius
+pub type Temperature = f32;
+/// Liters
+pub type Volume = f32;
+/// Minutes
+pub type Time = f32;
