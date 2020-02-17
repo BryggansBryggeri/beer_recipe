8b574304a18d900049937b8b56875c299701666c
Jakob <jakob.lindqvist@chalmers.se>
2020-02-17 08:11:55 +0100

    carbonation process added

--- src/bryggio/process/mod.rs
+++ src/bryggio/process/mod.rs
@@ -1,9 +1,11 @@
 pub(crate) mod boil;
+pub(crate) mod carbonation;
 pub(crate) mod fermentation;
 pub(crate) mod mash;
 pub(crate) mod preparation;
 
 pub(crate) use boil::Boil;
+pub(crate) use carbonation::Carbonation;
 pub(crate) use fermentation::Fermentation;
 pub(crate) use mash::Mash;
 //pub(crate) use preparation::Preparation;
--- src/bryggio/recipe.rs
+++ src/bryggio/recipe.rs
@@ -28,21 +28,14 @@ pub struct Recipe {
     yeasts: Vec<bryggio::Yeast>,
     waters: Vec<bryggio::Water>,
     miscs: Vec<bryggio::Misc>,
-    boil: process::Boil,
     mash: process::Mash,
+    boil: process::Boil,
     fermentation: process::Fermentation,
+    carbonation: process::Carbonation,
     notes: Option<String>,
     taste_notes: Option<String>,
     taste_rating: Option<f32>,
     pub(crate) date: Option<String>,
-    pub(crate) carbonation: Option<VolumesCO2>,
-    #[serde(default)]
-    #[serde(deserialize_with = "utils::opt_bool_de_from_str")]
-    pub(crate) forced_carbonation: Option<bool>,
-    pub(crate) priming_sugar_name: Option<String>,
-    pub(crate) carbonation_temp: Option<Celsius>,
-    pub(crate) priming_sugar_equiv: Option<f32>,
-    pub(crate) keg_priming_factor: Option<f32>,
 }
 
 impl From<beerxml::Recipe> for Recipe {
@@ -63,21 +56,16 @@ impl From<beerxml::Recipe> for Recipe {
             miscs: beerxml_recipe.miscs.misc,
             yeasts: beerxml_recipe.yeasts.yeast,
             waters: beerxml_recipe.waters.water,
+            mash: process::Mash {},
             boil: process::Boil {
                 volume: beerxml_recipe.boil_size,
             },
-            mash: process::Mash {},
             fermentation: process::Fermentation {},
+            carbonation: process::Carbonation {},
             notes: beerxml_recipe.notes,
             taste_notes: beerxml_recipe.taste_notes,
             taste_rating: beerxml_recipe.og,
             date: beerxml_recipe.date,
-            carbonation: beerxml_recipe.carbonation,
-            forced_carbonation: beerxml_recipe.forced_carbonation,
-            priming_sugar_name: beerxml_recipe.priming_sugar_name,
-            carbonation_temp: beerxml_recipe.carbonation_temp,
-            priming_sugar_equiv: beerxml_recipe.priming_sugar_equiv,
-            keg_priming_factor: beerxml_recipe.keg_priming_factor,
         }
     }
 }
